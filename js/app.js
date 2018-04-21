84bb1b56a472884872e7c55f04007b8585b13615
kiriki1991 <36364669+kiriki1991@users.noreply.github.com>
2018-04-21 11:10:04 +0300

    + get post by hash

--- js/app.js
+++ js/app.js
@@ -2,17 +2,14 @@
 golos.config.set('websocket', 'wss://ws.testnet.golos.io');
 golos.config.set('address_prefix', 'GLS');
 golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
-
-function auth() {
-	var username = 'test4';
-	var password = 'qwerty12345';
-	golos.api.login(username, password, function (err, result) {
-		//console.log(err, result);
-		if (!err) {
-			console.log('login', result);
-		} else console.error(err);
-	});
-}
+var	hash = location.hash.substring(1), // geting hash
+	resultContent = ''; // global variable for content
+if (hash != '') getHash();
+window.onhashchange = function () {
+	hash = location.hash.substring(1);
+	console.log('hash has been changed: ', hash);
+	if (hash != '') getHash();
+};
 
 function getTransaction(callback) {
 	console.log('<f> getTransaction');
@@ -20,10 +17,10 @@ function getTransaction(callback) {
 	var $div = document.createElement('tr'); // inserting header in poll 
 	$div.innerHTML = `
           <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Mark</td>
+          <td>` + resultContent.permlink + `</td>
+          <td>` + resultContent.author + `</td>
+          <td>@XYZ</td>
+          <td>` + resultContent.body + `</td>
           <td>Otto</td>
           <td>@mdo</td>
         </tr>`;
@@ -31,34 +28,10 @@ function getTransaction(callback) {
 	if (callback) callback();
 }
 
-function send_request(str, title, jsonMetadata) {
-	console.log('<f> send_request');
-	var parentAuthor = ''; // for post creating, empty field
-	var parentPermlink = 'test'; // main tag
-	var body = 'At the moment, you are looking at the test page of a simple microservice, which is currently under development. And since it so happened that you look at it, here`s a random cat, good luck to you and all the best.<img src="https://tinygrainofrice.files.wordpress.com/2013/08/kitten-16219-1280x1024.jpg"></img>'; // post text
-	golos.broadcast.comment(wif, parentAuthor, parentPermlink, username, str, title, body, jsonMetadata, function (err, result) {
-		//console.log(err, result);
-		if (!err) {
-			//console.log('post: ', result);
-			window.location.hash = username + '/' + str;
-			document.querySelector('.lding').style.display = 'none';
-		} else {
-			console.error(err);
-			swal({
-				type: 'error',
-				title: 'error',
-				text: err
-			});
-		}
-	}); // add post
-}
-
 function getHash() {
 	console.log('<f> getHash');
-	if (location.hash == '');
-	var startTarget = '/@'; // search '/@' - FIX THIS BUG! WHY IT`S WORKING?
+	var startTarget = '/@';
 	var startPos = -1;
-	document.querySelector('.lding').style.display = 'block';
 	while ((startPos = hash.indexOf(startTarget, startPos + 1)) != -1) {
 		var Pos = startPos,
 			targetStart = startPos;
@@ -73,10 +46,10 @@ function getHash() {
 		console.log(err, result);
 		resultContent = result;
 		if (!err && result.title != '') {
-			console.log('getContent', result.title);
+			console.log('gethash->getTransaction =', result.title);
+			getTransaction();
 		} else {
 			console.error('Failed to find post', err);
 		}
-		document.querySelector('.lding').style.display = 'none';
 	});
 }
