a48d695374ea40f6f0570001e81ed595a9937ccc
kiriki1991 <36364669+kiriki1991@users.noreply.github.com>
2018-05-01 13:54:08 +0300

    + merchant_global_update

--- inject.js
+++ inject.js
@@ -3,103 +3,90 @@ var golosJs, momentJs, sweetAlert, gAuth, gPollsApi, bootstrapMin, gPollsStyle, 
 bootstrapMin = document.createElement('link');
 bootstrapMin.rel = 'stylesheet';
 bootstrapMin.type = 'text/css';
-bootstrapMin.href = 'inject.css';
+bootstrapMin.href = 'https://golospolls.com/inject.css';
 (document.head || document.documentElement).appendChild(bootstrapMin);
 
+golosJs = document.createElement('script');
+golosJs.src = 'https://cdn.jsdelivr.net/npm/golos-js@0.6.3/dist/golos.min.js';
+(document.head || document.documentElement).appendChild(golosJs);
+
+momentJs = document.createElement('script');
+momentJs.src = 'https://cdn.jsdelivr.net/npm/moment@2.21.0/min/moment.min.js';
+(document.head || document.documentElement).appendChild(momentJs);
+
 sweetAlert = document.createElement('script');
-sweetAlert.src = 'https://unpkg.com/sweetalert2@7.15.0/dist/sweetalert2.all.js';
+sweetAlert.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@7.19.1/dist/sweetalert2.all.min.js';
 (document.head || document.documentElement).appendChild(sweetAlert);
 
-golosJs = document.createElement('script');
-golosJs.src = 'https://cdn.jsdelivr.net/npm/golos-js@0.6.1/dist/golos.min.js';
-(document.head || document.documentElement).appendChild(golosJs);
+bootstrapJs = document.createElement('script');
+bootstrapJs.src = 'https://cdn.jsdelivr.net/npm/bootstrap.native@2.0.22/dist/bootstrap-native-v4.min.js';
+(document.head || document.documentElement).appendChild(bootstrapJs);
 
 gAuth = document.createElement('script');
 gAuth.src = 'https://golosimages.com/auth.js';
 (document.head || document.documentElement).appendChild(gAuth);
 
-glang = document.createElement('script');
-glang.src = 'https://golosimages.com/lang.js';
-(document.head || document.documentElement).appendChild(glang);
+gApi = document.createElement('script');
+gApi.src = 'https://golospolls.com/api.js';
+(document.head || document.documentElement).appendChild(gApi);
 
-window.onload = function() { // init script after page loaded
-gPollsContainer = document.createElement('div');
-gPollsContainer.className = 'card border-primary mb-3';
-gPollsContainer.innerHTML = `<div class="card-header"><img src="bee-logo.ico" width="25" height="25" class="d-inline-block align-top" alt=""><a href="https://http://beesocial.in/" target="_blank">BeeSocial.in</a></div><div class="card-header-right"><p></p></div><div class="card-body text-dark"><img class="social-img" src="bee-logo.ico" style="width: 100%; height: 65%;"></div></div>`;
-document.querySelector('.gMerch').style.width = gWidth;
-document.querySelector('.gMerch').appendChild(gPollsContainer);
-
-golos.config.set('websocket', 'wss://ws.testnet.golos.io');
-golos.config.set('address_prefix', 'GLS');
-golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
-
-document.querySelector('.social-img').addEventListener('mouseover', () => {
-	console.log('<f> img over');
-	document.querySelector('.social-img').src = 'merch.jpg';
-}, false);
-	
-document.querySelector('.social-img').addEventListener('mouseout', () => {
-	console.log('<f> img out');
-	document.querySelector('.social-img').src = 'bee-logo.ico';
-}, false);
-	
-document.querySelector('.social-img').addEventListener('click', () => {
-	console.log('<f> progress_click');
-	if (wif) {
-		swal({ // visual 
-			type: 'success',
-			title: 'Thanks for making your choice!',
-			toast: true,
-			showConfirmButton: false,
-			timer: 2500
-		})
-		sendVote();
-	} else {
-		auth(() => {
-			swal({ // visual 
-				type: 'success',
-				title: 'Thanks for making your choice!',
-				toast: true,
-				showConfirmButton: false,
-				timer: 2500
-			})
-			sendVote();
-		});
-		if (err) {
-			swal({
-				title: 'error',
-				text: 'authorization failed',
-				type: 'error'
-			})
+function incertHtmlPoll(resultContent) {
+	console.log('<f> incertHtmlPoll ');
+	document.querySelector('.card-body.text-dark').innerHTML = '';
+	var $div = document.createElement('h5'); // inserting header in poll
+	$div.className = 'card-title';
+	$div.innerHTML = resultContent.json_metadata.data.poll_title;
+	document.querySelector('.card-body.text-dark').appendChild($div);
+	getVote(function () {
+		for (var cnt = 0; resultContent.json_metadata.data.poll_answers.length > cnt; cnt++) { // inserting progress 
+			var $div = document.createElement('div');
+			$div.className = 'progress-block';
+			if (resultContent.json_metadata.data.poll_answers[cnt]) {
+				$div.innerHTML = `<label class="card-text">` + resultContent.json_metadata.data.poll_answers[cnt] + `</label>
+				<div class="progress" id="` + cnt + `" style="cursor: pointer;">
+					<div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0</div>
+				</div><br>`;
+				document.querySelector('.card-body.text-dark').appendChild($div);
+				document.getElementById(cnt).onclick = progress_click; // dummy for polling 
+			} else {
+				$div.innerHTML = `<label class="card-text">` + resultContent.json_metadata.data.poll_answers[cnt] + `</label>
+				<div class="progress" id="` + cnt + `" style="cursor: pointer;">
+					<div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0</div>
+				</div><br>`;
+				document.querySelector('.card-body.text-dark').appendChild($div);
+				document.getElementById(cnt).onclick = progress_click; // dummy for polling     
+			}
 		}
-	}
-}, false);
-
-function sendVote(pollId) {
-	console.log('<f> sendVote');
-	var from = document.querySelector('#constr').querySelectorAll('.form-control')[0].value;
-	var to = document.querySelector('#constr').querySelectorAll('.form-control')[1].value;
-	var wif = document.querySelector('#constr').querySelectorAll('.form-control')[2].value;
-	var amount = document.querySelector('#constr').querySelectorAll('.form-control')[3].value;
-	var memo = document.querySelector('#constr').querySelectorAll('.form-control')[4].value;
-	golos.broadcast.transfer(wif, from, to, amount, memo, function (err, result) {
-		//console.log(err, result);
-		if (!err) {
-			swal({
-					title: 'success',
-					text: result,
-					type: 'success',
-					showConfirmButton: false,
-					timer: 2500
-			})
-		} else swal({
-					title: 'error',
-					text: err,
-					type: 'error',
-					showConfirmButton: false,
-					timer: 2500
-			});
-	})
+		getVote(function () {
+			console.log('<f> incertPollProg pollData', pollData);
+			cnt = resultContent.json_metadata.data.poll_answers.length;
+			for (index = 0; index < resultContent.json_metadata.data.poll_answers.length; ++index) {
+				if (typeof pollData[index] != 'undefined') {
+					pollData[index].percnt = Math.round((pollData[index].count * 100) / cnt);
+					if (document.querySelectorAll('.progress-bar')[index]) {
+						document.querySelectorAll('.progress-bar')[index].style = 'width: ' + pollData[index].percnt + '%;';
+						document.querySelectorAll('.progress-bar')[index].innerHTML = pollData[index].percnt + '% (' + pollData[index].count + ')';
+					}
+				}
+			}
+			document.querySelector('.card-header-right p').innerHTML = '</span><span class="badge badge-info">' + moment(resultContent.created).format('lll') + '</span>';
+		})
+	});
 }
 
-};
+window.addEventListener('load', function () { // init script after page loaded
+	console.log('<f> doc loaded');
+	golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
+	golos.config.set('websocket', 'wss://ws.testnet.golos.io');
+	gPollsContainer = document.createElement('div');
+	gPollsContainer.className = 'card border-primary mb-3';
+	gPollsContainer.innerHTML = `
+<div class="card-header bg-transparent border-success"><img src="https://golospolls.com/graphics/logo.png" width="25" height="25" class="d-inline-block align-top" alt=""><a href="https://golospolls.com/">GolosPolls</a></div><div class="card-header-right"><p></p></div><div class="card-body text-dark"></div></div>`;
+	document.querySelector('.gPolls').style.width = gPollsWidth;
+	document.querySelector('.gPolls').appendChild(gPollsContainer); // div inject
+	console.log('<f> doc ready');
+	hash = gPollsLink;
+	getHash(function (resultContent) {
+		incertHtmlPoll(resultContent);
+	});
+});
